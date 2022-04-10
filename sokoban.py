332a4b8facf005e7f3b970e7aac709736486ccd0
Jesus Bautista <97202824+JesusBHZ@users.noreply.github.com>
2022-04-10 05:26:19 +0000

    Movimiento moverDerecha creado y probado

--- sokoban.py
+++ sokoban.py
@@ -99,11 +99,105 @@ class Sokoban:
       os.system('cls')
     else:
       os.system('clear')
+
+  def moverDerecha(self):
+    #5.- Personaje, espacio 
+    if self.mapa[self.muneco_fila,self.muneco_columna]== 0 and self.mapa[self.muneco_fila,self.muneco_columna+1]==1:
+      self.mapa[self.muneco_fila,self.muneco_columna]=1
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=0
+      self.muneco_columna+=1
+     #6.-Personaje, meta    
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 0 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==4:
+      self.mapa[self.muneco_fila,self.muneco_columna]=1
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=5
+      self.muneco_columna+=1
+      #7.-Personaje, caja, espacio
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 0 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==2 and self.mapa[self.muneco_fila,self.muneco_columna+2]==1 :
+      self.mapa[self.muneco_fila,self.muneco_columna]=1
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=0
+      self.mapa[self.muneco_fila,self.muneco_columna+2]=2
+      self.muneco_columna+=1
+      #8.-Personaje, caja, meta
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 0 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==2 and self.mapa[self.muneco_fila,self.muneco_columna+2]==4 :
+      self.mapa[self.muneco_fila,self.muneco_columna]=1
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=0
+      self.mapa[self.muneco_fila,self.muneco_columna+2]=6
+      self.muneco_columna+=1
+      #9.-Personaje, caja_meta, espacio
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 0 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==6 and self.mapa[self.muneco_fila,self.muneco_columna+2]==1 :
+      self.mapa[self.muneco_fila,self.muneco_columna]=1
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=5
+      self.mapa[self.muneco_fila,self.muneco_columna+2]=2
+      self.muneco_columna+=1
+      #10.-Personaje, caja_meta, meta
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 0 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==6 and self.mapa[self.muneco_fila,self.muneco_columna+2]==4 :
+      self.mapa[self.muneco_fila,self.muneco_columna]=1
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=5
+      self.mapa[self.muneco_fila,self.muneco_columna+2]=6
+      self.muneco_columna+=1
+      #11.-Personaje_meta, espacio
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 5 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==1:
+      self.mapa[self.muneco_fila,self.muneco_columna]=4
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=0
+      self.muneco_columna+=1
+      #12.-Personaje_meta, meta
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 5 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==4:
+      self.mapa[self.muneco_fila,self.muneco_columna]=4
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=5
+      self.muneco_columna+=1
+      #13.-Personaje_meta, caja, espacio
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 5 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==2 and self.mapa[self.muneco_fila,self.muneco_columna+2]==1 :
+      self.mapa[self.muneco_fila,self.muneco_columna]=4
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=0
+      self.mapa[self.muneco_fila,self.muneco_columna+2]=2
+      self.muneco_columna+=1
+      #14.-Personaje_meta, caja, meta
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 5 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==2 and self.mapa[self.muneco_fila,self.muneco_columna+2]==4 :
+      self.mapa[self.muneco_fila,self.muneco_columna]=4
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=0
+      self.mapa[self.muneco_fila,self.muneco_columna+2]=6
+      self.muneco_columna+=1
+  
+      #15.-Personaje_meta, caja_meta, espacio
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 5 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==6 and self.mapa[self.muneco_fila,self.muneco_columna+2]==1 :
+      self.mapa[self.muneco_fila,self.muneco_columna]=4
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=5
+      self.mapa[self.muneco_fila,self.muneco_columna+2]=2
+      self.muneco_columna+=1
+        #16.-Personaje_meta, caja_meta, meta
+    elif self.mapa[self.muneco_fila,self.muneco_columna]== 5 and  self.mapa[self.muneco_fila,self.muneco_columna+1]==6 and self.mapa[self.muneco_fila,self.muneco_columna+2]==4 :
+      self.mapa[self.muneco_fila,self.muneco_columna]=4
+      self.mapa[self.muneco_fila,self.muneco_columna+1]=5
+      self.mapa[self.muneco_fila,self.muneco_columna+2]=6
+      self.muneco_columna+=1
+
    
 juego = Sokoban()#Crea un objeto para jugar
 juego.loadFile()
 juego.findColumnasFilas()
 juego.convertirFile()
 juego.findPosition()
-juego.printMap()
 juego.limpiar_pantalla
+
+while True:#Bucle para jugar N veces
+  intrucciones = "d-Derecha\ni-Izquierda\nr-Arriba\na-Abajo\nq-Salir" #Instrucciones
+  print(intrucciones)
+  print()
+  juego.printMap()#Imprime el mapa
+  movimientos = input("mover a: ")#Lee el movimiento
+  if movimientos == 'd':#si es d - mover a la derecha
+    juego.moverDerecha()#mueve el muñeco  a la derecha
+    juego.limpiar_pantalla()
+  elif movimientos == 'i': #si es a - mover a la izquierda
+    juego.moverIzquierda()#mueve el muñeco  a la izquierda
+    juego.limpiar_pantalla()
+  elif movimientos == 'r': #si es r - mover a arriba
+    juego.moverArriba()#mueve el muñeco  a arriba
+    juego.limpiar_pantalla()
+  elif movimientos == 'a': #si es l - mover a abajo
+    juego.moverAbajo()#mueve el muñeco  a abajo
+    juego.limpiar_pantalla()
+  elif movimientos == "q":#si es q-salir
+    print("Saliste del juego")#Imprmir mensaje
+    break #Rompe el ciclo while
+
