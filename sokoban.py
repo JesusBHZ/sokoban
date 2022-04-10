fcfdacc11380ee6c1cf001fa4e335561597e1c3f
Jesus Bautista <97202824+JesusBHZ@users.noreply.github.com>
2022-04-10 05:04:32 +0000

    Cargar Archivo desde una variable global

---
+++ sokoban.py
@@ -0,0 +1,40 @@
+"""
+Author: Jesus Bautista Hernandez
+Grupo: TI22
+      #0 = muñeco - 
+      #1 = espacio
+      #2 = caja - 
+      #3 = paredes
+      #4 = metas
+      #5 = muñeco_meta
+      #6 = caja_meta
+
+      intrucciones
+      d-Derecha
+      i-Izquierda
+      a-Arriba
+      h-Abajo
+      q-Salir
+"""
+import platform
+import os
+import  numpy as np
+class Sokoban:
+  archivo = ""
+  columnas = 0
+  filas = 0
+  mapa = []
+  nivel = 'level2.txt'
+
+  def __init__(self):
+        """_summary_: Constructor"""
+        pass
+    
+  def loadFile(self):
+    # Columnas
+    with open(self.nivel) as f:
+      colum = f.readline().rstrip()
+    self.columnas = len(colum)
+
+juego = Sokoban()#Crea un objeto para jugar
+juego.loadFile()
